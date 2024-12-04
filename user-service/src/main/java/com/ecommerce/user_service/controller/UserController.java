bb72cfb0b44467b432678a31616a72a36364bd59
vishnuvarma1996 <vishnuvarmavanapala@gmail.com>
2024-12-04 13:07:21 -0500

    Initial commit: Spring Boot project setup

---
+++ user-service/src/main/java/com/ecommerce/user_service/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.ecommerce.user_service.controller;
+
+import com.ecommerce.user_service.entity.User;
+import com.ecommerce.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id){
+        userRepository.deleteById(id);
+        return "User deleted with ID: " + id;
+    }
+
+}
---
+++ user-service/src/main/java/com/ecommerce/user_service/entity/User.java
@@ -0,0 +1,43 @@
+package com.ecommerce.user_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
